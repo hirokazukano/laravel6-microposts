0c812a57f910265307bc3a6e5d6084bbba325561
HirokazuKano <benzyamin2000@gmail.com>
2020-07-18 11:50:40 +0900

    Fix Laravle Mix導入

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,12 @@
     <title>@yield('title', config('app.name'))</title>
     <meta name="description" content="@yield('description', config('app.name') . 'です。')">
     <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+    @if (App::environment('local'))
+        <link rel="stylesheet" href="{{ asset('css/style1.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/style2.css') }}">
+    @else
+        <link rel="stylesheet" href="{{ asset('css/index.min.css') }}">
+    @endif
 </head>
 <body>
 
@@ -22,5 +28,11 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"></script>
 <script defer src="https://use.fontawesome.com/releases/v5.7.2/js/all.js"></script>
+@if (App::environment('local'))
+    <script src="{{ asset('js/js1.js') }}"></script>
+    <script src="{{ asset('js/js2.js') }}"></script>
+@else
+    <script src="{{ asset('js/index.min.js') }}"></script>
+@endif
 </body>
 </html>
--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,19 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+mix
+  .js(['public/js/js1.js', 'public/js/js2.js'], 'public/js/index.min.js')
+  .styles(['public/css/style1.css', 'public/css/style2.css'], 'public/css/index.min.css')
+  .browserSync({
+    files: [
+      './**/*.php',
+      './**/**/*.php',
+      './**/**/**/*.php',
+      './public/css/*.css',
+      './public/js/*.js',
+      './public/img/*.*',
+    ],
+    proxy: {
+      target: '192.168.33.29/github/techacademy/laravel6/microposts6/public',
+    }
+  });
